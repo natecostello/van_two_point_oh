5a236e149c62241792d1acc5f8dbaf34325a50f7
Nate Costello <natecostello@gmail.com>
2021-03-13 21:12:36 -0500

    added script to genenrate svg and png of flow chart

---
+++ project/build_flow_chart/export.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/Applications/draw.io.app/Contents/MacOS/draw.io -x -f svg -o Van_Flow_Chart.svg Van_Flow_Chart.drawio
+/Applications/draw.io.app/Contents/MacOS/draw.io -x -f png -t -o Van_Flow_Chart.png Van_Flow_Chart.drawio
