69e6b1e4254499d800ac774e506ed7c5258a30ff
Nate Costello <natecostello@gmail.com>
2021-04-11 10:02:52 -0400

    updated flow chart

--- project/build_flow_chart/export.sh
+++ project/build_flow_chart/export.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-/Applications/draw.io.app/Contents/MacOS/draw.io -x -f svg -o Van_Flow_Chart.svg Van_Flow_Chart.drawio
-/Applications/draw.io.app/Contents/MacOS/draw.io -x -f png -t -o Van_Flow_Chart.png Van_Flow_Chart.drawio
+/Applications/draw.io.app/Contents/MacOS/draw.io -x -f svg *.drawio
+/Applications/draw.io.app/Contents/MacOS/draw.io -x -f png -t *.drawio
+/Applications/draw.io.app/Contents/MacOS/draw.io -x -f jpg *.drawio
